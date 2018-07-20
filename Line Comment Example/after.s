c2bc3044b4ee87aea48680da7c418a65d2f6b950
Thomas Roskewich <roskewic@ualberta.ca>
2018-07-20 14:57:39 -0600

    Added comment fixer example

---
+++ Line Comment Example/after.s
@@ -0,0 +1,9 @@
+.data
+message: .asciiz "Hello World!"
+
+.text
+main:
+  la $a0, message  # Load helloworld string into register $a0
+  li $v0 4         # Load print string syscommand
+  syscall          # Print the string
+  jr $ra           # Return
